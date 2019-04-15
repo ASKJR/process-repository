4e0e22dc3466b49d4d27e6baf1cf162dfe159d94
ASKJR <albertokatojr@gmail.com>
2019-04-14 23:41:15 -0300

    adding file from pc to server - test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged in! <a href="{{asset('storage/failureType.pdf')}}"> Download </a>
                 </div>
             </div>
         </div>
